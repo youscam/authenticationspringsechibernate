1b8f3808c952b781b654a200736b4bd8e38393eb
kamal_azzouzi <youscam@outlook.com>
2017-06-17 02:35:50 +0000

    First commit

---
+++ src/main/java/ma/youscam/AuthenticationspringsechibernateApplication.java
@@ -0,0 +1,12 @@
+package ma.youscam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationspringsechibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationspringsechibernateApplication.class, args);
+	}
+}
